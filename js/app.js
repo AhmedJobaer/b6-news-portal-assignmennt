f00af2c27f7bad870767e94765330490f4d20c04
jobaer-ahmed <ajobaer08@gmail.com>
2022-09-03 00:22:08 +0800

    api load and se cetagor name in console

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('hellow')
+const loadData = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data.data.news_category))
+}
+
+const displayData = categories => {
+    console.log(categories);
+    categories.forEach(c => {
+        console.log(c.category_name);
+    })
+}
+
+loadData();
