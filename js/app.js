6a4046d8d6d4b891623b9d3aaeca2afad2308cb3
jobaer-ahmed <ajobaer08@gmail.com>
2022-09-04 03:17:39 +0800

    resposive done for small divcse

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const displayData = categories => {
     const categoryDiv = document.getElementById('category-link');
     categories.forEach(category => {
         const categoryDivList = document.createElement('div');
-        categoryDivList.classList.add('inline');
+        categoryDivList.classList.add('lg:inline', 'flex', 'flex-col', 'items-center');
         categoryDivList.innerHTML = `
             <ul class="menu menu-horizontal bg-base-100 rounded-box p-2">
             <li class="m-2" ><a onclick="category(${category.category_id})">${category.category_name}</a></li>
@@ -34,25 +34,31 @@ const category = (id) => {
 
 }
 
+
+
 const showCategoryDetail = (datas) => {
     const categoryListDivData = document.getElementById('category-list');
     categoryListDivData.innerHTML = ``;
     const totalInfo = document.getElementById('total-info');
     totalInfo.innerHTML = ``;
     totalInfo.innerHTML = `<p class="bg-white text-1xl text-black rounded-lg m-5 p-4 font-bold">${datas.length ? datas.length : 'No'} items found for this category</p>`;
+
+    datas.sort(function (a, b) {
+        return b.total_view - a.total_view
+    });
     //console.log(datas);
     datas.forEach(data => {
         console.log(data);
         const cateDiv = document.createElement('div');
         cateDiv.classList.add('mb-4')
         cateDiv.innerHTML = `
-            <div class="card card-side bg-base-100  shadow-xl">
-                <figure ><img class="w-72 h-80" src="${data.thumbnail_url ? data.thumbnail_url : 'No Thumnail_Image Data Found'}"></figure>
+            <div class="card card-side bg-base-100 flex flex-col lg:flex-row shadow-xl">
+                <figure ><img class="lg:w-72 lg:h-80 w-fit" src="${data.thumbnail_url ? data.thumbnail_url : 'No Thumnail_Image Data Found'}"></figure>
                 <div class="card-body">
-                    // <h2 class="card-title">${data.title ? data.title : 'No title Data Found'}</h2>
+                    <h2 class="card-title">${data.title ? data.title : 'No title Data Found'}</h2>
                     <p>${data.details.slice(0, 250)}</p>
                     <p>${data.details.slice(250, 450)}...</p>
-                    <div class="flex items-center">
+                    <div class="flex items-center lg:flex-row flex-col">
                         <div class="avatar-group -space-x-6 mr-2">
                             <div class="avatar">
                                 <div class="w-12">
@@ -64,10 +70,10 @@ const showCategoryDetail = (datas) => {
                             <h3 class="font-bold">${data.author.name ? data.author.name : 'No Data Found'}</h3>
                             <p> ${data.author.published_date ? data.author.published_date : 'No Data Found'}</p>
                         </div>
-                        <i class="fa-regular fa-eye ml-40 text-1xl"></i>
-                        <h3 class="font-bold ml-3">${data.total_view ? data.total_view : 'No Data Found'}</h3>
-                        <div class="card-actions justify-end">
-                            <label for="my-modal-3" class="btn btn-primary modal-button ml-60" onclick="newsDetail('${data._id ? data._id : 'No Data Found'}')">Show Details</label>
+                        <i class="fa-regular fa-eye mt-2 lg:ml-40 text-1xl"></i>
+                        <h3 class="font-bold mt-2 lg:ml-3">${data.total_view ? data.total_view : 'No Data Found'}</h3>
+                        <div class="card-actions mt-2 justify-end">
+                            <label for="my-modal-3" class="btn btn-primary modal-button lg:ml-60" onclick="newsDetail('${data._id ? data._id : 'No Data Found'}')">Show Details</label>
                         </div>
                     </div>
                     
@@ -105,13 +111,13 @@ const displayNews = news => {
     news.forEach(data => {
         //console.log(data);
         modalcontainer.innerHTML = `
-        <h3 class="text-lg font-bold">Title: ${data.title ? data.title : 'No Data Found'}</h3>
-        <h3 class="text-lg font-bold">Name: ${data.author.name ? data.author.name : 'No Data Found'}</h3>
+        <h3 class="text-lg font-bold mb-2">Title: ${data.title ? data.title : 'No Data Found'}</h3>
+        <h3 class="text-lg font-bold mb-2">Name: ${data.author.name ? data.author.name : 'No Data Found'}</h3>
         <p>Publish Date : ${data.author.published_date ? data.author.published_date : 'No data Found'}</p>
         <p>Rating Number : ${data.rating.number ? data.rating.number : 'No data Found'}</p>
         <p>Rating Badge : ${data.rating.badge ? data.rating.badge : 'No data Found'}</p>
-        <p>Total Views : ${data.total_view ? data.total_view : 'No data Found'}</p>
-        <img src="${data.image_url ? data.image_url : 'No Data Found'}"/>
+        <p class="mb-2">Total Views : ${data.total_view ? data.total_view : 'No data Found'}</p>
+        <img src="${data.author.img ? data.author.img : 'No Data Found'}"/>
         `;
 
     })
